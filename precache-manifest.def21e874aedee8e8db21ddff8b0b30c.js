cee178beb97cbd0b8845f3660abe0aa71b512d5e
juanda3111 <you@example.com>
2020-02-04 15:51:32 -0500

    Updates

---
+++ precache-manifest.def21e874aedee8e8db21ddff8b0b30c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acbf383b6a9560300e94132abc1f526f",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "90b126fc97abc876b611",
+    "url": "/personal-portfolio/static/css/main.27688927.chunk.css"
+  },
+  {
+    "revision": "b34b3a98ba701d1fc19c",
+    "url": "/personal-portfolio/static/js/2.9b8f62ab.chunk.js"
+  },
+  {
+    "revision": "90b126fc97abc876b611",
+    "url": "/personal-portfolio/static/js/main.16d5ce20.chunk.js"
+  },
+  {
+    "revision": "199df5cd20e1a679bb0f",
+    "url": "/personal-portfolio/static/js/runtime~main.8fd806ec.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/personal-portfolio/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/personal-portfolio/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/personal-portfolio/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/personal-portfolio/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "/personal-portfolio/static/media/bg4.4bff5d45.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/personal-portfolio/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/personal-portfolio/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/personal-portfolio/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/personal-portfolio/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/personal-portfolio/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "4a8ab91920048122a44232e907dadea4",
+    "url": "/personal-portfolio/static/media/foto-personal-baja-res.4a8ab919.png"
+  },
+  {
+    "revision": "ffcc99c42164b65da7af6307b66379cf",
+    "url": "/personal-portfolio/static/media/header.ffcc99c4.jpg"
+  },
+  {
+    "revision": "138381e51c42397348a82b448b5fb61b",
+    "url": "/personal-portfolio/static/media/julie.138381e5.jpg"
+  },
+  {
+    "revision": "f8fba45478e18757e67c37b6644a3cec",
+    "url": "/personal-portfolio/static/media/landing.f8fba454.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/personal-portfolio/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/personal-portfolio/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/personal-portfolio/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/personal-portfolio/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/personal-portfolio/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "225653f9f4dacef3e81bcc52f5ef547d",
+    "url": "/personal-portfolio/static/media/profile.225653f9.jpg"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/personal-portfolio/static/media/ryan.f217f346.jpg"
+  }
+]);
